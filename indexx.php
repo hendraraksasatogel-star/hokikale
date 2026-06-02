27c4118c47d6be901b5f33f73341b8e0ee5538e9
hendraraksasatogel-star <hendra.raksasatogel@gmail.com>
2026-06-02 13:44:40 +0700

    Create indexx.php

---
+++ indexx.php
@@ -0,0 +1,72 @@
+
+<!DOCTYPE html>
+<html lang="idn">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-param" content="_csrf">
+    <meta name="csrf-token" content="Q0dMR1l2b0wwPR12PiEfOyIAFigpJDsZKSopATRHHQ53MiYJMTtbGw==">
+    <title>Portal Aplikasi Perpustakaan Politeknik ATI Makassar</title>
+    <!-- Favicons -->
+    <link rel="shortcut icon" type='image/x-icon' href="/../favicon.png">
+    <link href="/assets/52d3d1c8/css/bootstrap.css" rel="stylesheet">
+<link href="/frontend/assets/css/site.css" rel="stylesheet">
+<link href="/frontend/assets/css/tab.css" rel="stylesheet"></head>
+<body>
+
+<div class="wrap">
+    
+    <div class="container">
+                        
+<div class="container">
+    <div class="row">
+
+    <center>
+        <img class="thing" src="/uploaded_files/aplikasi/inlislite.png" alt="Portal Aplikasi Inlis Lite" style="width:38%; margin-bottom:34px;">
+        <p>
+            <a class="btn btn-primary btn3d" target="_blank" href="backend"> Back Office</a>
+            <a class="btn btn-primary btn3d" target="_blank" href="bacaditempat"> Baca ditempat</a>
+            <a class="btn btn-primary btn3d" target="_blank" href="guestbook"> Buku Tamu</a>
+            <!--<a class="btn btn-primary btn3d" target="_blank" href="keanggotaan"> Keanggotaan Online</a>-->
+            <a class="btn btn-primary btn3d" target="_blank" href="digitalcollection"> Layanan Koleksi Digital</a>
+            <a class="btn btn-primary btn3d" target="_blank" href="opac"> OPAC</a>
+            <a class="btn btn-primary btn3d" target="_blank" href="article"> Artikel</a>
+            <!--<a class="btn btn-primary btn3d" target="_blank" href="pendaftaran"> Pendaftaran Anggota</a>-->
+            <a class="btn btn-primary btn3d" target="_blank" href="statistik-perkembangan-perpustakaan"> Statistik</a>          
+            <a class="btn btn-primary btn3d" target="_blank" href="survey"> Survey</a>
+            
+            <!--<a class="btn btn-primary btn3d" target="_blank" href="pengembalianmandiri"> Pengembalian Mandiri</a>-->
+            <!--<a class="btn btn-primary btn3d" target="_blank" href="peminjamanmandiri"> Peminjaman Mandiri</a>-->
+        </p>
+
+     </center>
+    </div>
+</div>
+
+<div class="site-index">
+    <center>
+        <div class="mediumtron">
+             <p>
+               
+             </p>
+
+        </div>
+    </center>
+    <div class="body-content">
+    </div>
+</div>
+    </div>
+</div>
+
+    <footer class="footer main-footer">
+        <div class="container">
+            <div class="pull-right hidden-sm" style="font-family: "Corbel", Arial, Helvetica, sans-serif;">
+            INLISLite v3.2        </div>
+        Hak Cipta  &copy; 2017 - 2018 <a href="http://inlislite.perpusnas.go.id" target="_blank">Perpustakaan Nasional Republik Indonesia</a>
+</div> <!-- /.container -->
+</footer>
+
+<script src="/assets/afec1196/jquery.js"></script>
+<script src="/assets/1786e7e5/yii.js"></script>
+</body>
+</html>
